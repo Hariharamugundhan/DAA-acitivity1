9205ca32b1a50adde26296ff368d5debad330652
Hariharamugundhan <hariharamugundhan433@gmail.com>
2026-04-16 20:19:45 +0530

    Implement multiple algorithm functions in activity_1.py

---
+++ activity_1.py
@@ -0,0 +1,65 @@
+# Two sum
+def twoSum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+
+# Longest substring without repeating characters
+def lengthOfLongestSubstring(s):
+    char_set = set()
+    left = 0
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
+
+# Product of array except self
+def productExceptSelf(nums):
+    n = len(nums)
+    res = [1] * n
+
+    prefix = 1
+    for i in range(n):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        res[i] *= suffix
+        suffix *= nums[i]
+
+    return res
+
+# Maximum subarray
+def maxSubArray(nums):
+    current = max_sum = nums[0]
+
+    for num in nums[1:]:
+        current = max(num, current + num)
+        max_sum = max(max_sum, current)
+
+    return max_sum
+
+# Binary search
+def search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
